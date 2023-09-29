b305c16cd5c68f2f47379ead01b513b0403a4cc3
ChinoManda <francofedullo22@gmail.com>
2023-09-29 08:54:04 -0300

    Create Tarea con validator

--- app/Http/Controllers/TareasController.php
+++ app/Http/Controllers/TareasController.php
@@ -4,17 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\tarea;
+use Illuminate\Support\Facades\Validator;
 class TareasController extends Controller
 {
-    public function RegisterCreate (Request $request){
-        $User = new user();
+
+    public function CreateTarea(Request $request){
         
-        $User -> name = $request ->post("name"); 
-        $User -> surname = $request ->post("surname");
-        $User -> age = $request ->post("age");
-        $User -> email = $request ->post("email");
-        $User -> password = Hash::make($request -> post("password"));
-        $User -> save();       
-        return $User;
+        $validation = self::CreateTareaValidation($request);
+
+        if ($validation->fails())
+        return $validation->errors();
+    
+        return $this -> CreateTareaRequest($request);
+    }
+
+    public function CreateTareaValidation(Request $request){
+        $validation = Validator::make($request->all(),[
+            'titulo' => 'required | string ',
+            'contenido' => 'required | string',
+            'estado' => 'required',
+            'autor' => 'required',
+        ]);
+        return $validation;    
+    }
+
+    public function CreateTareaRequest (Request $request){
+        $Tarea = new tarea();
+        
+        $Tarea -> titulo = $request ->post("titulo"); 
+        $Tarea -> contenido = $request ->post("contenido");
+        $Tarea -> estado = $request ->post("estado");
+        $Tarea -> autor = $request ->post("autor");
+        $Tarea -> save();       
+        return $Tarea;
     }
 }
