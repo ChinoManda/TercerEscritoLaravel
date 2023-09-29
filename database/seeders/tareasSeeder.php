f88f1c133d6b329ea2d67c1ae4fa79b7ae5ba177
ChinoManda <francofedullo22@gmail.com>
2023-09-29 09:31:11 -0300

    Seeder terminado con tests

--- database/seeders/tareasSeeder.php
+++ database/seeders/tareasSeeder.php
@@ -3,16 +3,18 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 class tareasSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        DB::table('tareas')->insert([
+            'titulo' => "hola",
+            'contenido' => "adios",
+            'estado' => "soltero",
+            'autor' => "yo",
+        ]);
     }
 }
